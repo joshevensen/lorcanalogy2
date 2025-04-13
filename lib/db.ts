ff1a75577c01eba517bc2cea56a7efa75b0b0ef9
Josh Evensen <joshevensen@outlook.com>
2025-04-13 09:57:48 -0500

    switch back to zenstack

--- lib/db.ts
+++ lib/db.ts
@@ -1,9 +1,11 @@
 import { PrismaClient } from '@prisma/client';
-// import { enhance } from '@zenstackhq/runtime';
+import { enhance } from '@zenstackhq/runtime';
 // import { getSessionUser } from './auth';
 
 const prisma = new PrismaClient();
-// const db = enhance(prisma);
+const db = enhance(prisma);
 // const db = enhance(prisma, { user: getSessionUser() });
 
-export default prisma;
+export default db;
+
+export const runtime = 'edge';
